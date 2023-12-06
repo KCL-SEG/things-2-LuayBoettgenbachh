babfa9f7c2200dfd7743bb40c89c5e3085201d73
Luay Boettgenbach <luay.boettgenbach@kcl.ac.uk>
2023-12-06 23:33:08 +0000

    Add code for things2

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,14 @@
 """Forms of the project."""
+from django import forms
+from things.models import Thing
 
 # Create your forms here.
+
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        exclude = ['created_at']
+
+    # Customize the widgets for specific fields
+    description = forms.CharField(widget=forms.Textarea(attrs={'maxlength': 120}))
+    quantity = forms.IntegerField(widget=forms.NumberInput)
